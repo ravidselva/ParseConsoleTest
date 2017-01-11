a89e40b3bddc274d6f95990053ee28436d483371
Ravi Kumar <ravi.kumar@dselva.co.in>
2017-01-11 13:45:16 +0530

    changes commited

--- Parsedoc_Console/Parsedoc_Console/Program.cs
+++ Parsedoc_Console/Parsedoc_Console/Program.cs
@@ -12,16 +12,13 @@ namespace Parsedoc_Console
         static void Main(string[] args)
         {          
             var obj = new EventItemModel();
-            const string fileName = "UnitTest.docx";
+            const string fileName = "Маршруты движения (только первый вход и последний выход).docx";
             var fileLocation = new FileInfo(fileName).FullName;
             var empData = obj.LoadCollection(fileLocation);
             
             foreach (var emp in empData)
             {
-
-                Console.WriteLine(emp.DateTime + "|" + emp.Door + "|" + string.Empty + "|" + emp.Details.ToString());
-                
-                //Console.WriteLine(String.Format("Date/Time = {0}, Door = {1}, EventType = {2}, Description = {3}", emp.DateTime, emp.Door, string.Empty, emp.Details));
+                Console.WriteLine("Date/Time = {0}, Door = {1}, EventType = {2}, Description = {3}", emp.DateTime, emp.Door, string.Empty, emp.Details);
             }
 
             Console.WriteLine("Press Enter to Exit");
